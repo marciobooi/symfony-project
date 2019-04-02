39059256f66785de3732b56b7b559303e955efe4
marcio1978 <78marcio@gmail.com>
2019-04-02 16:01:22 +0200

    add navbar footer and created new pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,42 +6,33 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class DefaultController
 {
 
     /**
+     *
      * @Route("/", name="homepage")
      * @return Response
+     * @throws \Twig\Error\ErrorLoader
+     * @throws \Twig\Error\RuntimeError
+     * @throws  \Twig\Error\SyntaxError
      */
-    public function homepageAction()
+
+    public function homepageAction(Environment $twig)
     {
-        return new Response('<!DOCTYPE><html><header>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</header>
+        $color = 'red';
 
-<style>
-
-
-</style>
-
-
-<body>
-
-
-
-
-<h1><a href="/terms">Terms</a></h1>
-
-
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>');
+        return new Response(
+            $twig->render(
+            'default/homepage.html.twig',
+                ['color' => $color
+                ]
+            )
+        );
     }
+
 
     /**
      *  @Route("/terms", name="Terms_of_service")
@@ -52,7 +43,7 @@ class DefaultController
         return new Response('
 <!DOCTYPE><html>
 <body>
-Terms of service... <a href="/">Home</a>
+Terms of service... <a href="/whichName/public/">Home</a>
 </body>
 </html> ');
     }
